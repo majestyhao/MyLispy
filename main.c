5164cdf138b22ba846322bf602fd8b596b02b4d9
Hao Fu <jamesfuhao@gmail.com>
2014-12-21 10:56:07 -0800

    error handleling

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 static char buffer[2048];
 
-        char*readline(char*prompt){
-        fputs(prompt,stdout);
-        fgets(buffer,2048,stdin);
-        char*cpy=malloc(strlen(buffer)+1);
-        strcpy(cpy,buffer);
-        cpy[strlen(cpy)-1]='\0';
+char* readline(char* prompt){
+        fputs(prompt, stdout);
+        fgets(buffer, 2048, stdin);
+        char *cpy = malloc(strlen(buffer)+1);
+        strcpy(cpy, buffer);
+        cpy[strlen(cpy) - 1]='\0';
         return cpy;
         }
 
@@ -40,26 +40,88 @@ int number_of_nodes(mpc_ast_t *t) {
 	}
 }
 
-/* Use operator string to see which operator to perform */
-long eval_op(long x, char *op, long y) {
-	if (strcmp(op, "+") == 0) return x + y;
-	if (strcmp(op, "-") == 0) return x - y;
-	if (strcmp(op, "*") == 0) return x * y;
-	if (strcmp(op, "/") == 0) return x / y;
-	return 0;
+/* Declare New lval Struct */
+typedef struct {
+	int type;
+	long num;
+	int err;
+}lval;
+
+/* Create Enumeration of Possiible lval Types */
+enum {LVAL_NUM, LVAL_ERR};
+
+/* Create Enumberation of Possible Error Types */
+enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
+
+/* Create a new number type 1val */
+lval lval_num(long x) {
+	lval v;
+	v.type = LVAL_NUM;
+	v.num = x;
+	return v;
 }
 
-long eval(mpc_ast_t *t) {
+/* Create a new error type lval */
+lval lval_err(int x) {
+	lval v;
+	v.type = LVAL_ERR;
+	v.err = x;
+		return v;
+}
+
+/* Print an "lval" */
+void lval_print(lval v) {
+	switch (v.type) {
+		case LVAL_NUM: printf("%li", v.num); break;
+		case LVAL_ERR:
+					   if(v.err == LERR_DIV_ZERO) {
+						   printf("Error: Division by 0");
+					   }
+						if (v.err == LERR_BAD_OP) {
+							printf("ERROR: invalid operator!");
+						}
+							if (v.err == LERR_BAD_NUM) {
+								printf("ERROR: INVALID NUMBER!");
+							}
+							break;
+						}
+}
+
+void lval_println(lval v) {
+	lval_print(v);
+	putchar('\n');
+}
+
+/* Use operator string to see which operator to perform */
+lval eval_op(lval x, char *op, lval y) {
+	/* If either value is an error return it */
+	if (x.type == LVAL_ERR) return x;
+	if (y.type == LVAL_ERR) return y;
+
+	if (strcmp(op, "+") == 0) return lval_num(x.num + y.num);
+	if (strcmp(op, "-") == 0) return lval_num(x.num - y.num);
+	if (strcmp(op, "*") == 0) return lval_num(x.num * y.num);
+	if (strcmp(op, "/") == 0) {
+	/* If sec operand is 0 return error */
+	return y.num == 0 ? lval_err(LERR_DIV_ZERO):lval_num(x.num / y.num);
+	}
+	return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t *t) {
 	/* If tagged as number return it directly. */
 	if (strstr(t -> tag, "number")) {
-		return atoi(t -> contents);
+		/* Check if there is some error in conversion */
+		errno = 0;
+		long x = strtol(t -> contents, NULL, 10);
+		return errno != ERANGE ? lval_num(x): lval_err(LERR_BAD_NUM);
 	}
 
 	/* The operator is always second child. */
 	char *op = t -> children[1] -> contents;
 
 	/* We store the third child in 'x' */
-	long x = eval(t -> children[2]);
+	lval x = eval(t -> children[2]);
 
 	/* Iterate the remaining children and combining. */
 	int i = 3;
@@ -71,7 +133,6 @@ long eval(mpc_ast_t *t) {
 
 	return x;
 }
-
 
         int main(int argc,char**argv)
         {	    
@@ -119,8 +180,8 @@ mpca_lang(MPCA_LANG_DEFAULT,
 	//printf("num of nodes %i\n", number_of_nodes(&r));
 
 				/* Printing the eval result */
-	long result = eval(r.output);
-	printf("%li\n", result);
+	lval result = eval(r.output);
+	lval_println(result);
 	mpc_ast_delete(r.output);
 
 		 /* On Success Pring the AST */
